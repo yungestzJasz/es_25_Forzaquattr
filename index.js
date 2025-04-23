b12e4d600f30e47f02e6f47444be80bdca33fc56
yungestzJasz <j.singh.3617@vallauri.edu>
2025-04-23 10:54:37 +0200

    aggiunta sceletro
    aggiunta java css html

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const RIGHE=6;
+const COLONNE=7;
+
+const GREY = "rgb(127, 127, 127)";
+const YELLOW = "rgb(255, 255, 0)";
+const RED = "rgb(255, 0, 0)";
+
+
+window.onload=function(){
+
+ 
+
+}
