188cef988ddebbb2989fc441fec8f43b394e42fc
yungestzJasz <j.singh.3617@vallauri.edu>
2025-04-23 11:40:36 +0200

    aggunta tabella

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ const RED = "rgb(255, 0, 0)";
 
 
 window.onload=function(){
+  creaTabella();
+  turno();
 
- 
-
+}
+function creaTabella(){
+    let wrapper= document.getElementById("wrapper");
+    for(let i=0;i<RIGHE;i++){
+        let riga=document.createElement("div");
+        for (let j = 0; j <COLONNE; j++)
+        {
+           let cella=document.createElement("div");
+           cella.setAttribute("id",`cella${i}${j}`);
+            cella.classList.add("pedina");
+            wrapper.appendChild(cella);
+        }        
+    }
+   
+}
+function turno(){
+    let prossimoturnoTxt=document.getElementById("nextPlayer");
+    
 }
